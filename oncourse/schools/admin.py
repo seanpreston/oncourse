7870a0c48a7bba6c1046b7181eaee55302a7a134
seanpreston <seanmpreston@gmail.com>
2014-05-09 19:29:41 +0100

    Added admin to Schools app

--- oncourse/schools/admin.py
+++ oncourse/schools/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import School, Location
+
+
+class SchoolAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+admin.site.register(School, SchoolAdmin)
+
+
+class LocationAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+admin.site.register(Location, LocationAdmin)
