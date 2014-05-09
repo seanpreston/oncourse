2511c054fdac21243789a5f67117048f8a7f7a5e
seanpreston <seanmpreston@gmail.com>
2014-05-09 19:30:26 +0100

    Added URL for schools detail

--- oncourse/schools/api_urls.py
+++ oncourse/schools/api_urls.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
 
-from .api import SchoolsListView
+from .api import SchoolsListView, SchoolsView
 
 urlpatterns = patterns(
     '',
 
     url(r'api/v0/schools/$', SchoolsListView.as_view(), name='api-v0-schools'),
+    url(r'api/v0/schools/(?P<school_slug>[\w\d_.-]+)/$', SchoolsView.as_view(), name='api-v0-schools'),
 
 )
