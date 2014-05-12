f0efd1fb892c61c34b71cda443a6d9cf06d4b486
seanpreston <seanmpreston@gmail.com>
2014-05-12 17:15:25 +0100

    Amended @authenticate wrapper until auth is added

--- oncourse/api/decorators.py
+++ oncourse/api/decorators.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 
 def authenticate(fn):
     def wrapper(self, request, *args, **kwargs):
+        # TODO: Remove after auth is added
+        return fn(self, request, *args, **kwargs)
         if not self.is_authenticated(request):
             return HttpResponse('Unauthorized', status=401)
 
